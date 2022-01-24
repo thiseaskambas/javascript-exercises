6b737d91ba9138df0dd43c4f1acce2826a393256
thiseaskambas <tkambas@yahoo.com>
2022-01-24 14:41:54 -0400

    added simplier solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function(str) {
 
+
+const reverseString = function (str) {
+   const temp = Array.from(str);
+   let newStr = '';
+   for (let i = temp.length - 1; i >= 0; i--) {
+      newStr += temp[i]
+   }
+   return newStr;
 };
+
+/* simplier solution would have been as follows :
+
+const reverseString = function(string) {
+   return string.split('').reverse().join('')
+};
+*/
 
 // Do not edit below this line
 module.exports = reverseString;
