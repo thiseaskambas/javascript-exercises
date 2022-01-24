c7a9df5b0e2c00fdb8b810645a007aa7db19b3bc
thiseaskambas <tkambas@yahoo.com>
2022-01-24 16:05:25 -0400

    completed exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function (...args) {
+   args.sort();
+   if (typeof (args[0]) === 'number' &&
+      typeof (args[1]) === 'number' &&
+      args[0] >= 0 &&
+      args[1] >= 0) {
+      let sum = 0;
+      for (let i = args[0]; i <= args[1]; i++) {
+         sum += i
+      }
+      return sum;
+   } else {
+      return 'ERROR'
+   }
 
 };
 
+
+sumAll(10, [90, 1])
 // Do not edit below this line
 module.exports = sumAll;
