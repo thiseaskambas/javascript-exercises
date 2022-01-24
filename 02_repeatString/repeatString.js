0182b379800bf5ce8573d0642a2a94cf5cdf16ec
thiseaskambas <tkambas@yahoo.com>
2022-01-24 14:23:37 -0400

    completed the exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,22 @@
-const repeatString = function() {
 
+
+
+
+
+
+
+
+
+const repeatString = function (str, reps) {
+   if (reps < 0) {
+      return 'ERROR'
+   } else {
+      let newStr = ''
+      for (let i = 0; i < reps; i++) {
+         newStr += str
+      }
+      return newStr
+   }
 };
 
 // Do not edit below this line
