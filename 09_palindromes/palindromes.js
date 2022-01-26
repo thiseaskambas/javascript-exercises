4e272cd4635bf18ed04d9d810416206bcc8aa49e
thiseaskambas <tkambas@yahoo.com>
2022-01-26 16:52:52 -0400

    normalize input

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+   let lowStr = str.toLowerCase();
+   let arr = []
+   let newstr = '';
+   for (let i = 0; i < lowStr.length; i++) {
+      if (lowStr[i].match(/[a-z]/)) {
+         // arr.push(lowStr[i])
+         newstr += lowStr[i]
+      }
+   }
 
+   for (let i = 0; i < newstr.length / 2; i++) {
+      (newstr[i] === newstr[newstr.length - (1 + i)])
+   }
 };
 
 // Do not edit below this line
