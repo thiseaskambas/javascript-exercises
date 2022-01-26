47819e64d90caf0918539823fa25d7daf2b9a084
thiseaskambas <tkambas@yahoo.com>
2022-01-26 16:05:22 -0400

    completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((accumulator, current) => {
+    return accumulator + current
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((accumulator, current) => {
+    return accumulator * current
+  })
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let result = 1;
+  for (let i = 1; i <= number; i++) {
+    result = result * i
+  }
+  return result
 };
 
 // Do not edit below this line
