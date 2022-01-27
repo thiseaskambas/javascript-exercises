b9ded00aba1266c89c6b97e41e860bd66e5d9cf2
thiseaskambas <tkambas@yahoo.com>
2022-01-26 20:05:41 -0400

    solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+   if (num < 0) {
+      return 'OOPS'
+   }
+   let arr = [1, 1];
+   let next = 0;
 
+   for (let i = 1; i <= num; i++) {
+      next = arr[i] + arr[i - 1];
+      arr.push(next)
+   }
+   return arr[num - 1];
 };
 
 // Do not edit below this line
