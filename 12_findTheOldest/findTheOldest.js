48a6b56ad22f077be0acc3fe4fbd59d718db9460
thiseaskambas <tkambas@yahoo.com>
2022-01-26 20:37:09 -0400

    solved using arra.sort

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+   array.sort((first, second) => {
+      const ageFirst = (first.yearOfDeath || 2022) - first.yearOfBirth
+      const ageSecond = (second.yearOfDeath || 2022) - second.yearOfBirth
+      return ageSecond > ageFirst ? 1 : -1
+   })
+   return array[0]
 };
 
 // Do not edit below this line
