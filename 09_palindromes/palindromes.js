be6526a9c5330976337687eba4147e61570d6297
thiseaskambas <tkambas@yahoo.com>
2022-01-26 20:14:58 -0400

    added cleaner solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -26,5 +26,20 @@ const palindromes = function (str) {
    }
 };
 
+
+/* 
+//MUCH CLEANER SOLUTION:
+
+const palindromes = function(string) {
+  processedString = string.toLowerCase().replace(/[^A-Za-z]/g, "");
+  return (
+    processedString
+      .split("")
+      .reverse()
+      .join("") == processedString
+  );
+};
+*/
+
 // Do not edit below this line
 module.exports = palindromes;
